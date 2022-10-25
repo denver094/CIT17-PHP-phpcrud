69880dee98b6ceab58d6fc6557af9e50ddc14264
DenverRamos <72637006+denver094@users.noreply.github.com>
2022-10-25 17:21:39 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2 style="font-family: Courier New;">Home</h2>
+	<img src="homepage.jpg" style="padding: 100px;">
+</div>
+
+<?=template_footer()?>
